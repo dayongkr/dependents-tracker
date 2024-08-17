d6511862aa1c721cb26763d0a30fa88006988f42
dayongkr <dayongkr@gmail.com>
2024-08-17 22:26:41 +0900

    format: write prettier all files

--- packages/dependents-scrapper/rollup.config.js
+++ packages/dependents-scrapper/rollup.config.js
@@ -1,10 +1,10 @@
-import typescript from "@rollup/plugin-typescript";
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: "./src/index.ts",
+  input: './src/index.ts',
   output: {
-    file: "dist/index.js",
-    format: "es",
+    file: 'dist/index.js',
+    format: 'es',
     sourcemap: true,
   },
   plugins: [typescript()],
