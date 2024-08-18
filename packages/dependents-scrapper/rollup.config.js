08b06609cc3ec42b0da0b15a961ff694cd83d3c8
dayongkr <dayongkr@gmail.com>
2024-08-18 16:42:25 +0900

    chore(scrap): disable terser when is watch mode

--- packages/dependents-scrapper/rollup.config.js
+++ packages/dependents-scrapper/rollup.config.js
@@ -1,4 +1,7 @@
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+
+const isWatch = process.argv.includes('-w') || process.argv.includes('--watch');
 
 export default {
   input: './src/index.ts',
@@ -11,6 +14,7 @@ export default {
     typescript({
       tsconfig: '../../tsconfig.json',
     }),
+    !isWatch && terser(),
   ],
   external: ['node:fs', 'node:child_process', 'node:path', 'node:url'],
 };
