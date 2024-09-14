2bd575c85fe54d17075faa032c3997897c1c9051
dayongkr <dayongkr@gmail.com>
2024-09-14 15:56:28 +0900

    refactor(extractor): implement functions to extract import declarations and specifiers, add unit tests

--- packages/extractor/src/parse.ts
+++ packages/extractor/src/parse.ts
@@ -1,1 +1,19 @@
-export {};
+export function getImportDeclarations(line: string, packageName: string): ReturnType<typeof String.prototype.match> {
+  const regex = new RegExp(`import[^'"]*['"]${packageName}`, 'gs');
+  return line.match(regex);
+}
+
+export function getImportSpecifiers(importDeclaration: string): string[] {
+  const regex = /{[^'"]+}/g;
+  const matches = importDeclaration.match(regex);
+
+  if (matches === null) {
+    return [];
+  }
+
+  const unwrapped = matches[0].slice(1, -1);
+  const specifiers = unwrapped.split(',').map((specifier) => specifier.trim().split(' ')[0]);
+  const filtered = specifiers.filter((specifier) => !specifier.includes('/'));
+
+  return filtered;
+}
---
+++ packages/extractor/test/parse.spec.ts
@@ -0,0 +1,69 @@
+import { describe, it, expect } from 'vitest';
+import { getImportDeclarations, getImportSpecifiers } from '../src/parse';
+
+describe('getImportDeclarations', () => {
+  const lines = [
+    'import defaultExport from "module-name";',
+    'import * as name from "module-name";',
+    'import { export1 } from "module-name";',
+    'import { export1 as alias1 } from "module-name";',
+    'import { default as alias } from "module-name";',
+    'import { export1, export2 } from "module-name";',
+    'import { export1, export2 as alias2, /* … */ } from "module-name";',
+    'import defaultExport, { export1, /* … */ } from "module-name";',
+    'import defaultExport, * as name from "module-name";',
+    'import "module-name";',
+  ];
+
+  const linesInOneString = `
+  import defaultExport from "module-name";
+  import * as name from "module-name";
+  import { export1 } from "module-name";
+  import { export1 as alias1 } from "module-name";
+  import { default as alias } from "module-name";
+  import { export1, export2 } from "module-name";
+  import { export1, export2 as alias2, /* … */ } from "invalid";
+  import defaultExport, { export1, /* … */ } from "module-name";
+  import defaultExport, * as name from "invalid";
+  import "module-name";
+  `;
+
+  lines.forEach((line) => {
+    it(`should get import lines for ${line}`, () => {
+      const matches = getImportDeclarations(line, 'module-name');
+      if (matches === null) {
+        throw new Error('No matches found');
+      }
+      expect(matches[0]).toBe(line.slice(0, -2));
+    });
+  });
+
+  it('should get import lines for all lines in one string', () => {
+    const matches = getImportDeclarations(linesInOneString, 'module-name');
+    if (matches === null) {
+      throw new Error('No matches found');
+    }
+    expect(matches).toHaveLength(8);
+  });
+});
+
+describe('getImportSpecifiers', () => {
+  it('should get import specifiers', () => {
+    expect(getImportSpecifiers('import { export1 } from "module-name";')).toEqual(['export1']);
+    expect(getImportSpecifiers('import { export1 as alias1 } from "module-name";')).toEqual(['export1']);
+    expect(getImportSpecifiers('import { default as alias } from "module-name";')).toEqual(['default']);
+    expect(getImportSpecifiers('import { export1, export2 } from "module-name";')).toEqual(['export1', 'export2']);
+    expect(getImportSpecifiers('import { export1, export2 as alias2, /* … */ } from "module-name";')).toEqual([
+      'export1',
+      'export2',
+    ]);
+    expect(getImportSpecifiers('import defaultExport, { export1, /* … */ } from "module-name";')).toEqual(['export1']);
+  });
+
+  it('should return empty array for invalid import lines', () => {
+    expect(getImportSpecifiers('import * as name from "module-name";')).toEqual([]);
+    expect(getImportSpecifiers('import defaultExport from "module-name";')).toEqual([]);
+    expect(getImportSpecifiers('import defaultExport, * as name from "module-name";')).toEqual([]);
+    expect(getImportSpecifiers('import "module-name";')).toEqual([]);
+  });
+});
