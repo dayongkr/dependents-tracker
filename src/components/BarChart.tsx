f2b4b190697f4421f372462d098baa6b9eabd294
dayongkr <dayongkr@gmail.com>
2024-09-17 14:28:31 +0900

    feat(app): add BarChart and DonutChart components for improved data visualization

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Bar, BarChart as BarChartRecharts, CartesianGrid, LabelList, XAxis } from 'recharts';
+
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/shadcn/card';
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/shadcn/chart';
+
+export function BarChart<T>({
+  data,
+  title,
+  description,
+  xDataKey,
+  yDataKey,
+  label,
+}: Readonly<{
+  data: T[];
+  title: string;
+  description: string;
+  xDataKey: string & keyof T;
+  yDataKey: string & Omit<keyof T, typeof xDataKey>;
+  label?: string;
+}>) {
+  const chartConfig = {
+    [yDataKey]: {
+      label: label,
+      color: 'hsl(var(--chart-1))',
+    },
+  } satisfies ChartConfig;
+
+  return (
+    <Card className="flex flex-col lg:col-span-3">
+      <CardHeader>
+        <CardTitle className="text-xl">{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex w-full flex-1 flex-col justify-center">
+        <ChartContainer config={chartConfig}>
+          <BarChartRecharts
+            accessibilityLayer
+            data={data}
+            margin={{
+              top: 20,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis dataKey={xDataKey} tickLine={false} tickMargin={10} axisLine={false} />
+            {label && <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />}
+            <Bar dataKey={yDataKey} fill={`var(--color-${yDataKey})`} radius={8}>
+              <LabelList position="top" offset={12} className="fill-foreground" fontSize={12} />
+            </Bar>
+          </BarChartRecharts>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
