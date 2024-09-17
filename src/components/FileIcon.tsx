b140bac243d2969c17c7e7f816b9940c22b8c942
dayongkr <dayongkr@gmail.com>
2024-09-17 16:14:56 +0900

    refactor(app): replace logos icons with vscode-icons

--- src/components/FileIcon.tsx
+++ src/components/FileIcon.tsx
@@ -1,21 +1,25 @@
-import LogosTypescriptIcon from '~icons/logos/typescript-icon';
-import LogosJavascript from '~icons/logos/javascript';
-import LogosVue from '~icons/logos/vue';
-import LogosSvelteIcon from '~icons/logos/svelte-icon';
-import LogosMarkdown from '~icons/logos/markdown';
-import { FileQuestion } from 'lucide-react';
+import VscodeIconsFileTypeTypescriptOfficial from '~icons/vscode-icons/file-type-typescript-official';
+import VscodeIconsFileTypeJsOfficial from '~icons/vscode-icons/file-type-js-official';
+import VscodeIconsFileTypeVue from '~icons/vscode-icons/file-type-vue';
+import VscodeIconsFileTypeReactjs from '~icons/vscode-icons/file-type-reactjs';
+import VscodeIconsFileTypeReactts from '~icons/vscode-icons/file-type-reactts';
+import VscodeIconsFileTypeSvelte from '~icons/vscode-icons/file-type-svelte';
+import VscodeIconsFileTypeMarkdown from '~icons/vscode-icons/file-type-markdown';
+import VscodeIconsDefaultFile from '~icons/vscode-icons/default-file';
 
 const IconMap = new Map<string, (props: React.SVGProps<SVGSVGElement>) => React.ReactElement>();
 
-['ts', 'tsx', 'mts', 'cts'].forEach((ext) => IconMap.set(ext, LogosTypescriptIcon));
-['js', 'jsx', 'mjs', 'cjs'].forEach((ext) => IconMap.set(ext, LogosJavascript));
-['vue'].forEach((ext) => IconMap.set(ext, LogosVue));
-['svelte'].forEach((ext) => IconMap.set(ext, LogosSvelteIcon));
-['md', 'markdown', 'mdx'].forEach((ext) => IconMap.set(ext, LogosMarkdown));
+['ts', 'mts', 'cts'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeTypescriptOfficial));
+['js', 'mjs', 'cjs'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeJsOfficial));
+['vue'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeVue));
+['jsx'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeReactjs));
+['tsx'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeReactts));
+['svelte'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeSvelte));
+['md', 'markdown', 'mdx'].forEach((ext) => IconMap.set(ext, VscodeIconsFileTypeMarkdown));
 
 export function FileIcon({ filename }: { filename: string }) {
   const ext = filename.toLowerCase().split('.').pop() ?? '';
-  const Icon = IconMap.get(ext) ?? FileQuestion;
+  const Icon = IconMap.get(ext) ?? VscodeIconsDefaultFile;
 
   return <Icon width={20} height={20} />;
 }
