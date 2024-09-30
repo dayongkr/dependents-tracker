9b34cfcec07264e3492385d9fe55b778ecc47f96
dayongkr <dayongkr@gmail.com>
2024-09-30 19:24:50 +0900

    refactor(components): add className prop to Section component for enhanced flexibility

--- apps/web/src/components/Section.tsx
+++ apps/web/src/components/Section.tsx
@@ -1,6 +1,12 @@
-export function Section({ children, title }: Readonly<{ children: React.ReactNode; title: string }>) {
+import { cn } from '@/libs/utils';
+
+export function Section({
+  children,
+  title,
+  className,
+}: Readonly<{ children: React.ReactNode; title: string; className?: string }>) {
   return (
-    <section className="mb-12 mt-6 flex flex-col gap-6">
+    <section className={cn('mb-12 mt-6 flex flex-col gap-6', className)}>
       <h2 className="text-xl font-semibold">{title}</h2>
       {children}
     </section>
